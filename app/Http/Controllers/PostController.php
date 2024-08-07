2369162484d7c7e9a611bd0cfc58585dc0e82844
hasegawa-ikumi <m22a182hi@takachiho.ac.jp>
2024-08-07 16:10:19 +0900

    ８-6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,6 +43,14 @@ $post->save();
             $post->fill($input_post)->save();
             return redirect('/posts/' . $post->id);
         }
+        
+        public function delete(Post $post)
+        {
+            
+            $post->delete();
+            return redirect('/');
+            
+        }
 }
 
 
